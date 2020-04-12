1ff3fc102f7f46a8a9411ba3c5fc718b33025873
jonathandash <jonathan.dash@scionresearch.com>
2020-04-12 15:42:21 +0100

    Update the als non tiled script

--- als_wall2wall_nontiled.R
+++ als_wall2wall_nontiled.R
@@ -13,7 +13,7 @@ library(raster)
 
 
 # Read data
-ctg<-readLAS('C:/paper2-Tekapo/code/data/plane-als-study-site/balmoral_merged.laz')
+ctg<-readLAS('C:/paper2-Tekapo/code/data/plane-als-full-study-site/BALMORAL-merged.laz')
 
 
 
@@ -27,7 +27,27 @@ las<-lasfilter(ctg, Z>0)
 
 #opt_output_files(ctg) <- ""
 
-metrics_w2w <- grid_metrics(ctg, .stdmetrics, res = 1)
+#dtm <- grid_terrain(las, algorithm = knnidw(k = 8, p = 2))
+dtm <- grid_terrain(las, algorithm = tin())
+las_normalized <- lasnormalize(las, dtm)
+
+# Create a filter to remove points above 95th percentile of height
+lasfilternoise = function(las, sensitivity)
+{
+  p95 <- grid_metrics(las, ~quantile(Z, probs = 0.95), 10)
+  las <- lasmergespatial(las, p95, "p95")
+  las <- lasfilter(las, Z < p95*sensitivity)
+  las$p95 <- NULL
+  return(las)
+}
+
+las_denoised <- lasfilternoise(las_normalized, sensitivity = 1.2)
+
+
+
+metrics_w2w <- grid_metrics(las_denoised, .stdmetrics, res = 1)
 names(metrics_w2w)
+
+#crs(metrics_w2w)
 
 writeRaster(metrics_w2w, here('out', 'wsw_als_metrics_hanmer.tif'), overwrite = TRUE)
